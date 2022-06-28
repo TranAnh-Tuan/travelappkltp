4cf268c225598446a00c5a9e0dce4d12f6df20c5
Bui Xuan Khoi <khoibuiqn1011@gmail.com>
2022-06-29 02:04:02 +0700

    commit 1

---
+++ tlkp/src/main/java/com/travelapp/tlkp/TlkpApplication.java
@@ -0,0 +1,13 @@
+package com.travelapp.tlkp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlkpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlkpApplication.class, args);
+	}
+
+}
---
+++ tlkp/src/test/java/com/travelapp/tlkp/TlkpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelapp.tlkp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TlkpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
