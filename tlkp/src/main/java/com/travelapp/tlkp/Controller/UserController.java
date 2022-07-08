e1c76c3d0c2994cb3e8bd57e414e646cf58ef241
Bui Xuan Khoi <khoibuiqn1011@gmail.com>
2022-07-08 22:34:39 +0700

    Update UserController.java

--- tlkp/src/main/java/com/travelapp/tlkp/Controller/UserController.java
+++ tlkp/src/main/java/com/travelapp/tlkp/Controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.travelapp.tlkp.Service.UserService;
@@ -33,7 +34,7 @@ public class UserController {
 	
 	
 	@GetMapping("/detail")
-	public User getUserByEmail(@RequestBody String userEmail) {
+	public User getUserByEmail(@RequestParam(name = "email", required = false, defaultValue = "khoi@gmail.com") String userEmail) {
 		return this.userService.findUserByEmail(userEmail);
 	}
 	
